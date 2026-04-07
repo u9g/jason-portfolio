0e162a4cd08bef7e6b562646af525a4013c223b6
U9G <git@u9g.dev>
2026-04-07 14:48:50 -0400

    feat: Show entry logo beside conversation title

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -2,6 +2,7 @@
 import PromptBar from "./PromptBar.vue";
 import ShareButton from "./ShareButton.vue";
 import { renderMessage } from "../data/render-markdown";
+import { entryLogos, darkInvertLogos } from "../data/entry-logos";
 
 defineProps<{
   title: string;
@@ -19,6 +20,13 @@ const emit = defineEmits<{ toggleSidebar: [] }>();
       <button v-if="sidebarCollapsed" class="sidebar-toggle" @click="emit('toggleSidebar')">
         <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M16.5 4C17.3284 4 18 4.67157 18 5.5V14.5C18 15.3284 17.3284 16 16.5 16H3.5C2.67157 16 2 15.3284 2 14.5V5.5C2 4.67157 2.67157 4 3.5 4H16.5ZM7 15H16.5C16.7761 15 17 14.7761 17 14.5V5.5C17 5.22386 16.7761 5 16.5 5H7V15ZM3.5 5C3.22386 5 3 5.22386 3 5.5V14.5C3 14.7761 3.22386 15 3.5 15H6V5H3.5Z"></path></svg>
       </button>
+      <img
+        v-if="entryLogos[slug]"
+        :class="['title-logo', { 'title-logo--dark-invert': darkInvertLogos.has(slug) }]"
+        :src="entryLogos[slug]"
+        alt=""
+        aria-hidden="true"
+      />
       {{ title }}
       <ShareButton />
     </div>
@@ -74,6 +82,20 @@ const emit = defineEmits<{ toggleSidebar: [] }>();
 
 .sidebar-toggle:hover {
   background: var(--bg-hover);
+}
+
+.title-logo {
+  width: auto;
+  height: 18px;
+  max-width: 22px;
+  object-fit: contain;
+  flex-shrink: 0;
+  user-select: none;
+  -webkit-user-drag: none;
+}
+
+:root[data-theme="dark"] .title-logo--dark-invert {
+  filter: invert(1);
 }
 
 @media (min-width: 1025px) {
